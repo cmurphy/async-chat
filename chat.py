c47300a69fa834e232cf2a57829b45306171731e
Colleen Murphy <colleen@gazlene.net>
2019-06-26 14:44:44 -0700

    Allow multiple peers to connect

--- chat.py
+++ chat.py
@@ -15,14 +15,15 @@ async def receive_message(reader, writer):
         print(data.decode('utf-8'))
 
 
-async def send_message(reader, writer):
+async def send_message(writers):
     print("Waiting for input")
     stdin, _ = await aioconsole.get_standard_streams()
     while True:
         data = await stdin.read(100)
         if not data:
             break
-        writer.write(data)
+        for writer in writers:
+            writer.write(data)
 
 
 async def listen(port):
@@ -31,20 +32,22 @@ async def listen(port):
         await server.serve_forever()
 
 
-async def connect(port):
-    reader, writer = (None, None)
-    while (reader, writer) == (None, None):
-        try:
-            reader, writer = await asyncio.open_connection('127.0.0.1', port)
-            await send_message(reader, writer)
-        except ConnectionRefusedError:
-            await asyncio.sleep(1)
+async def connect(ports):
+    writers = []
+    while len(writers) < len(ports):
+        for port in ports:
+            try:
+                _, writer = await asyncio.open_connection('127.0.0.1', port)
+                writers.append(writer)
+            except ConnectionRefusedError:
+                await asyncio.sleep(1)
+    await send_message(writers)
 
 
 async def main():
     on = sys.argv[1]
-    to = sys.argv[2]
-    await asyncio.gather(listen(on), connect(to))
+    peers = sys.argv[2].split(',')
+    await asyncio.gather(listen(on), connect(peers))
 
 
 if __name__ == '__main__':
